34913f763221b1d09e0b47d025bb365a4b6f766b
clemcke <clemcke@spokeo.com>
2013-02-05 12:14:21 -0800

    Updated readme for additional installation steps, and run_visualizer now loads an actual file.

--- run_visualizer.rb
+++ run_visualizer.rb
@@ -2,5 +2,5 @@ require 'visualizer'
 require 'effect'
 Dir["lib/*.rb"].each {|f| require f}
 
-viz = Visualizer.new :title => "Visualizer", :width => 200, :height => 200
-Sandbox2.new(viz)
+viz = Visualizer.new :title => "Visualizer", :width => 600, :height => 600
+BouncingCircles.new(viz)
